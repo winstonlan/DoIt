28fcec2409a4a0485516c8ceb3e805317e7bbef5
TGwonton <winston96lan@gmail.com>
2017-04-28 16:40:14 -0700

    Implemented Task Class with TableView.

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Winston Lan on 4/28/17.
+//  Copyright © 2017 winstonlan. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name  = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -11,22 +11,49 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var tasks: [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tasks = makeTasks()
+        
         tableView.dataSource = self
         tableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "hello"
+        let task = tasks[indexPath.row]
+        
+        
+        if tasks[indexPath.row].important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
         return cell
+    }
+    
+    func makeTasks() ->[Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow Lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+        
     }
 
 }
